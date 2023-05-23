e022e1f66b976b9b1f1a5c39f451ab3386594de3
developer64 <mdmahfuj010@gmail.com>
2023-05-23 21:19:31 +0600

    Update GraphQLService.dart

--- GraphQLService.dart
+++ GraphQLService.dart
@@ -1,1 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class GraphQLService {
+  static final String _baseUrl = 'https://example.com/graphql';
+
+  late Dio _dio;
+  late GraphQLClient _client;
+
+  GraphQLService() {
+    _dio = Dio();
+    _client = GraphQLClient(
+      cache: GraphQLCache(),
+      link: HttpLink(_baseUrl, httpClient: _dio),
+    );
+  }
+
+  Future<QueryResult> performQuery(String query, {Map<String, dynamic>? variables}) async {
+    final options = QueryOptions(
+      document: gql(query),
+      variables: variables,
+    );
+
+    final result = await _client.query(options);
+    if (result.hasException) {
+      throw result.exception!;
+    }
+
+    return result;
+  }
+}
 
